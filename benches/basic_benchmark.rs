15b1422e16ac94857487658da5871bc1255d93e8
Minh Tri Pham <minh.pham@epfl.ch>
2022-11-25 18:55:15 +0100

    Change numbers on benchmark

--- benches/basic_benchmark.rs
+++ benches/basic_benchmark.rs
@@ -29,14 +29,14 @@ fn write_to_table(table: &mut Table<u32, u32>, size: u32, write_proportion: f32)
 fn write_proportion_benchmark(c: &mut Criterion) {
     // Setup table
     let mut database: Database<u32, u32> = Database::new();
-    let size: u32 = 10000;
+    let size: u32 = 500000;
     let mut table = database.empty_table();
     setup_table(&mut table, size);
     // Benchmark
-    let write_proportions = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0];
+    let write_proportions = [0.0, 0.25, 0.5, 0.75, 1.0];
     let mut group = c.benchmark_group("Different write proportions");
     for wp in write_proportions {
-        group.bench_with_input(BenchmarkId::new("write_proportion", wp), &wp, |b, &wp| {b.iter(|| write_to_table(&mut table, size, wp))});
+        group.bench_with_input(BenchmarkId::new("write_proportion", wp), &wp, |b, &wp| {b.iter(|| write_to_table(&mut table, 500000, wp))});
     };
 }
 
@@ -44,13 +44,13 @@ fn write_proportion_benchmark(c: &mut Criterion) {
 fn no_operations_benchmark(c: &mut Criterion) {
     let mut database: Database<u32, u32> = Database::new();
     let mut table = database.empty_table();
-    setup_table(&mut table, 100000);
-    let no_ops = [1000, 10000, 100000];
+    setup_table(&mut table, 500000);
+    let no_ops = [50000, 125000, 250000, 500000];
     let mut group = c.benchmark_group("Different number of operations");
     for no_op in no_ops {
         group.bench_with_input(BenchmarkId::new("number_operations", no_op), &no_op, |b, &no_op| {b.iter(|| write_to_table(&mut table, no_op, 0.5))});
     }
 }
 
-criterion_group!(benches, write_proportion_benchmark, no_operations_benchmark);
+criterion_group!(benches, no_operations_benchmark, write_proportion_benchmark);
 criterion_main!(benches);
